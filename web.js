03383d600d86c7ed31d6eba0a1303b21e2b37b19
John Smith <example@stanford.edu>
2013-07-13 11:14:22 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
   var buffer = new Buffer (256);
-  buffer = new Buffer(fs.readFileSync, "utf-8")
+  buffer = new Buffer(fs.readFileSync("index.html"), "utf-8")
   response.send(buffer.toString('utf-8'));
 });
 var port = process.env.PORT || 5000;
